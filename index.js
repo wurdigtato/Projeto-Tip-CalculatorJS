4ee52b788337cf1cc80e8ff0397b4531ec4fc0a1
Tácio Würdig <wurdigtm@outlook.com>
2025-01-02 15:03:54 -0300

    footer add

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function update() {
     let splitCalc = totalBill / splitInput
 
 
-    tipValue.innerText = `R$ ${tipTotal.toFixed(2)}`
+    tipValue.innerText = `$ ${tipTotal.toFixed(2)}`
     tipPercent.innerText = `${tipInput}%`
     splitValue.innerText = `${splitInput} Person`
-    total.innerText = `R$ ${totalBill.toFixed(2)}`
-    eachBill.innerText = `R$ ${splitCalc.toFixed(2)}`
+    total.innerText = `$ ${totalBill.toFixed(2)}`
+    eachBill.innerText = `$ ${splitCalc.toFixed(2)}`
 
     console.log(totalBill)
 }
